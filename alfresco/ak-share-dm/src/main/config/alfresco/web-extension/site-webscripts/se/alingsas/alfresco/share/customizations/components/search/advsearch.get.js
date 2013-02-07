e79b5414ccc3286e4c98d155fb26c871194fcf68
Marcus Svensson <marcus.svensson@redpill-linpro.com>
2013-02-07 13:43:13 +0100

    #15029 - Fixed search issue so that advanced searches will search using sites home as base. Also fixed so that a user can leave document status and secrecy fields blank in searches.

---
+++ alfresco/ak-share-dm/src/main/config/alfresco/web-extension/site-webscripts/se/alingsas/alfresco/share/customizations/components/search/advsearch.get.js
@@ -0,0 +1,12 @@
+/**
+ * Advanced Search component GET method
+ */
+
+function main()
+{
+	//Force advanced searches to use sites as base
+	model.searchRepo = false;
+	// Prepare the model
+}
+
+main();
