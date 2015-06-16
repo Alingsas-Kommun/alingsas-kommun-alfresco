c4fd670f47bc0cfa505eef0bddda14ee2dc3a3cc
Marcus Svensson <marcus.svensson@redpill-linpro.com>
2015-06-16 10:53:11 +0200

    #22802 - Reimplemented Title-filename switch

--- alfresco/ak-share-dm/src/main/amp/web/ak/components/documentlibrary/ak-documentlist.js
+++ alfresco/ak-share-dm/src/main/amp/web/ak/components/documentlibrary/ak-documentlist.js
@@ -1,15 +1,15 @@
 (function(renderCellDescription) {
 
    Alfresco.DocumentListViewRenderer.prototype.renderCellDescription = function(scope, elCell, oRecord, oColumn, oData) {
+      if (this.name !== "table" && this.name !== "audio" && this.name !== "media_table") {
+         var record = oRecord.getData();
 
-      var record = oRecord.getData();
-
-      if (record.jsNode.properties.title !== null && record.jsNode.properties.title !== undefined && record.jsNode.properties.title !== "") {
-         var tmp = record.displayName;
-         record.displayName = record.jsNode.properties.title;
-         record.jsNode.properties.title = tmp;
+         if (record.jsNode.properties.title !== null && record.jsNode.properties.title !== undefined && record.jsNode.properties.title !== "") {
+            var tmp = record.displayName;
+            record.displayName = record.jsNode.properties.title;
+            record.jsNode.properties.title = tmp;
+         }
       }
-
       renderCellDescription.call(this, scope, elCell, oRecord, oColumn, oData);
    };
 
