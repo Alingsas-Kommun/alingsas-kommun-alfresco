be9b56b7801f577b83500b893579d4e587947efa
Marcus Svartmark <marcus.svartmark@redpill-linpro.com>
2015-08-21 13:30:06 +0200

    Fixed formatting in byggreda import tool, hid the view in browser action and upgraded findwise integration module.

--- alfresco/ak-repo-dm/src/main/java/se/alingsas/alfresco/repo/scripts/ByggRedaImport.java
+++ alfresco/ak-repo-dm/src/main/java/se/alingsas/alfresco/repo/scripts/ByggRedaImport.java
@@ -39,116 +39,106 @@ import org.springframework.extensions.webscripts.WebScriptRequest;
 
 import se.alingsas.alfresco.repo.utils.byggreda.ByggRedaUtil;
 
-public class ByggRedaImport extends DeclarativeWebScript implements
-		InitializingBean {
+public class ByggRedaImport extends DeclarativeWebScript implements InitializingBean {
 
-	private static final Logger LOG = Logger.getLogger(ByggRedaImport.class);
-	private static ServiceRegistry serviceRegistry;
-	private static String siteName;
-	private static String basePath;
+  private static final Logger LOG = Logger.getLogger(ByggRedaImport.class);
+  private static ServiceRegistry serviceRegistry;
+  private static String siteName;
+  private static String basePath;
 
-	@Override
-	protected Map<String, Object> executeImpl(final WebScriptRequest req,
-			final Status status, final Cache cache) {
-		final Map<String, Object> model = new HashMap<String, Object>();
-		// Check permissions since only site collaborators and managers should
-		// be able to run this action
+  @Override
+  protected Map<String, Object> executeImpl(final WebScriptRequest req, final Status status, final Cache cache) {
+    final Map<String, Object> model = new HashMap<String, Object>();
+    // Check permissions since only site collaborators and managers should
+    // be able to run this action
 
-		SiteService siteService = serviceRegistry.getSiteService();
-		MutableAuthenticationService authenticationService = serviceRegistry
-				.getAuthenticationService();
-		String currentUserName = authenticationService.getCurrentUserName();
-		String membersRole = siteService.getMembersRole(siteName,
-				currentUserName);
-		if ("SiteManager".equals(membersRole)
-				|| "SiteCollaborator".equals(membersRole)) {
-			// get the site parameter
-			final String sourcePath = basePath + "/"
-					+ req.getParameter("sourcePath");
-			final String metaFileName = req.getParameter("metaFileName");
-			final boolean updateExisting = "yes".equals(req
-					.getParameter("updateIfExists"));
+    SiteService siteService = serviceRegistry.getSiteService();
+    MutableAuthenticationService authenticationService = serviceRegistry.getAuthenticationService();
+    String currentUserName = authenticationService.getCurrentUserName();
+    String membersRole = siteService.getMembersRole(siteName, currentUserName);
+    if ("SiteManager".equals(membersRole) || "SiteCollaborator".equals(membersRole)) {
+      // get the site parameter
+      final String sourcePath = basePath + "/" + req.getParameter("sourcePath");
+      final String metaFileName = req.getParameter("metaFileName");
+      final boolean updateExisting = "yes".equals(req.getParameter("updateIfExists"));
 
-			File f = new File(sourcePath);
-			if (!f.exists()) {
-				//status.setCode(400);
-				//status.setMessage("Kan inte läsa källmappen " + sourcePath
-				//		+ ". Kontrollera att du skrivit in korrekt sökväg.");
-				//status.setRedirect(true);
-				status.setCode(200);
-				model.put("result", "Kan inte läsa källmappen " + sourcePath
-						+ ". Kontrollera att du skrivit in korrekt sökväg.");
-				return model;
-			}
-			f = new File(sourcePath + "/" + metaFileName);
-			if (!f.exists()) {
-				//status.setCode(400);
-				//status.setMessage("Kan inte läsa styrfilen " + sourcePath + "/"
-				//		+ metaFileName
-				//		+ ". Kontrollera att du skrivit in korrekt sökväg.");
-				//status.setRedirect(true);
-				status.setCode(200);
-				model.put("result", "Kan inte läsa styrfilen " + sourcePath + "/"
-						+ metaFileName
-						+ ". Kontrollera att du skrivit in korrekt sökväg.");
-				return model;
-			}
-			ByggRedaUtil bru = new ByggRedaUtil();
-			bru.setUpdateExisting(updateExisting);
-			bru.setSourcePath(sourcePath);
-			bru.setMetaFileName(metaFileName);
-			bru.setRunAs(AuthenticationUtil.getFullyAuthenticatedUser());
-			if (!bru.validateParams()) {
-				status.setCode(200);
-				//status.setMessage("Kunde inte validera inparametrar. Kontrollera systemloggen för mer detaljer.");
-				model.put("result", "Kunde inte validera inparametrar. Kontrollera systemloggen för mer detaljer.");
-				//status.setRedirect(true);
-			} else {
-				Thread t = new Thread(bru);
-				t.start();
-				status.setCode(200);
-				model.put("result", "Byggredaimport startad. Du kan nu stänga denna sidan.");
-			}
-		} else {
-			//status.setCode(400);
-			status.setCode(200);
-			model.put("result", "Åtkomst nekad, du måste vara ansvarig eller dokumentansvarig för samarbetsytan för att använda detta verktyg.");
-			//status.setMessage("Access denied, you must be site manager or site collaborator to run this tool");
-			//status.setRedirect(true);
-			return model;
-		}
+      File f = new File(sourcePath);
+      if (!f.exists()) {
+        // status.setCode(400);
+        // status.setMessage("Kan inte läsa källmappen " + sourcePath
+        // + ". Kontrollera att du skrivit in korrekt sökväg.");
+        // status.setRedirect(true);
+        status.setCode(200);
+        model.put("result", "Kan inte läsa källmappen " + sourcePath + ". Kontrollera att du skrivit in korrekt sökväg.");
+        return model;
+      }
+      f = new File(sourcePath + "/" + metaFileName);
+      if (!f.exists()) {
+        // status.setCode(400);
+        // status.setMessage("Kan inte läsa styrfilen " + sourcePath + "/"
+        // + metaFileName
+        // + ". Kontrollera att du skrivit in korrekt sökväg.");
+        // status.setRedirect(true);
+        status.setCode(200);
+        model.put("result", "Kan inte läsa styrfilen " + sourcePath + "/" + metaFileName + ". Kontrollera att du skrivit in korrekt sökväg.");
+        return model;
+      }
+      ByggRedaUtil bru = new ByggRedaUtil();
+      bru.setUpdateExisting(updateExisting);
+      bru.setSourcePath(sourcePath);
+      bru.setMetaFileName(metaFileName);
+      bru.setRunAs(AuthenticationUtil.getFullyAuthenticatedUser());
+      if (!bru.validateParams()) {
+        status.setCode(200);
+        // status.setMessage("Kunde inte validera inparametrar. Kontrollera systemloggen för mer detaljer.");
+        model.put("result", "Kunde inte validera inparametrar. Kontrollera systemloggen för mer detaljer.");
+        // status.setRedirect(true);
+      } else {
+        Thread t = new Thread(bru);
+        t.start();
+        status.setCode(200);
+        model.put("result", "Byggredaimport startad. Du kan nu stänga denna sidan.");
+      }
+    } else {
+      // status.setCode(400);
+      status.setCode(200);
+      model.put("result", "Åtkomst nekad, du måste vara ansvarig eller dokumentansvarig för samarbetsytan för att använda detta verktyg.");
+      // status.setMessage("Access denied, you must be site manager or site collaborator to run this tool");
+      // status.setRedirect(true);
+      return model;
+    }
 
-		return model;
-	}
+    return model;
+  }
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		// TODO Auto-generated method stub
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    // TODO Auto-generated method stub
 
-	}
+  }
 
-	public ServiceRegistry getServiceRegistry() {
-		return serviceRegistry;
-	}
+  public ServiceRegistry getServiceRegistry() {
+    return serviceRegistry;
+  }
 
-	public void setServiceRegistry(ServiceRegistry serviceRegistry) {
-		ByggRedaImport.serviceRegistry = serviceRegistry;
-	}
+  public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+    ByggRedaImport.serviceRegistry = serviceRegistry;
+  }
 
-	public String getSiteName() {
-		return siteName;
-	}
+  public String getSiteName() {
+    return siteName;
+  }
 
-	public void setSiteName(String siteName) {
-		ByggRedaImport.siteName = siteName;
-	}
+  public void setSiteName(String siteName) {
+    ByggRedaImport.siteName = siteName;
+  }
 
-	public String getBasePath() {
-		return basePath;
-	}
+  public String getBasePath() {
+    return basePath;
+  }
 
-	public void setBasePath(String basePath) {
-		ByggRedaImport.basePath = basePath;
-	}
+  public void setBasePath(String basePath) {
+    ByggRedaImport.basePath = basePath;
+  }
 
 }
