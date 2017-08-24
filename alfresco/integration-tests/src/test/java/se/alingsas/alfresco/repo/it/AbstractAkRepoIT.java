7b2136653ac88a070e9df68c1ba3b5a316c7b1bf
Jimmie Aleksic <jimmie.aleksic@gmail.com>
2017-08-24 16:11:34 +0200

    Fixed integration tests

---
+++ alfresco/integration-tests/src/test/java/se/alingsas/alfresco/repo/it/AbstractAkRepoIT.java
@@ -0,0 +1,11 @@
+package se.alingsas.alfresco.repo.it;
+
+import org.redpill.alfresco.test.AbstractRepoIntegrationTest;
+
+/**
+ *
+ * @author Marcus Svartmark - Redpill Linpro AB
+ */
+public abstract class AbstractAkRepoIT extends AbstractRepoIntegrationTest {
+  
+}
