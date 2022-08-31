90b884891846375bd076b1f1f6d7798fcf112878
Andreas Lindström-Åberg <andreas.lindstrom-aberg@redpill-linpro.com>
2022-08-31 12:46:54 +0200

    Replaced mutagen-run.sh with run.sh

---
+++ alfresco/run.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+
+export COMPOSE_FILE_PATH="${PWD}/target/classes/docker/docker-compose.yml"
+
+if [ -z "${M2_HOME}" ]; then
+  export MVN_EXEC="mvn"
+else
+  export MVN_EXEC="${M2_HOME}/bin/mvn"
+fi
+
+start() {
+    docker volume create alfresco-alingsas-acs-volume
+    docker volume create alfresco-alingsas-db-volume
+    docker volume create alfresco-alingsas-ass-volume
+    docker-compose -f "$COMPOSE_FILE_PATH" up --build -d
+}
+
+start_share() {
+    docker-compose -f "$COMPOSE_FILE_PATH" up --build -d alfresco-alingsas-share
+}
+
+start_acs() {
+    docker-compose -f "$COMPOSE_FILE_PATH" up --build -d alfresco-alingsas-acs
+}
+
+down() {
+    if [ -f "$COMPOSE_FILE_PATH" ]; then
+        docker-compose -f "$COMPOSE_FILE_PATH" down
+    fi
+}
+
+purge() {
+    docker volume rm -f alfresco-alingsas-acs-volume
+    docker volume rm -f alfresco-alingsas-db-volume
+    docker volume rm -f alfresco-alingsas-ass-volume
+}
+
+build() {
+    $MVN_EXEC clean package
+}
+
+build_share() {
+    docker-compose -f "$COMPOSE_FILE_PATH" kill alfresco-alingsas-share
+    yes | docker-compose -f "$COMPOSE_FILE_PATH" rm -f alfresco-alingsas-share
+    $MVN_EXEC clean package -pl alfresco-alingsas-share,alfresco-alingsas-share-docker
+}
+
+build_acs() {
+    docker-compose -f "$COMPOSE_FILE_PATH" kill alfresco-alingsas-acs
+    yes | docker-compose -f "$COMPOSE_FILE_PATH" rm -f alfresco-alingsas-acs
+    $MVN_EXEC clean package -pl alfresco-alingsas-integration-tests,alfresco-alingsas-platform,alfresco-alingsas-platform-docker
+}
+
+tail() {
+    docker-compose -f "$COMPOSE_FILE_PATH" logs -f
+}
+
+tail_all() {
+    docker-compose -f "$COMPOSE_FILE_PATH" logs --tail="all"
+}
+
+prepare_test() {
+    $MVN_EXEC verify -DskipTests=true -pl alfresco-alingsas-platform,alfresco-alingsas-integration-tests,alfresco-alingsas-platform-docker
+}
+
+test() {
+    $MVN_EXEC verify -pl alfresco-alingsas-platform,alfresco-alingsas-integration-tests
+}
+
+case "$1" in
+  build_start)
+    down
+    build
+    start
+    tail
+    ;;
+  build_start_it_supported)
+    down
+    build
+    prepare_test
+    start
+    tail
+    ;;
+  start)
+    start
+    tail
+    ;;
+  stop)
+    down
+    ;;
+  purge)
+    down
+    purge
+    ;;
+  tail)
+    tail
+    ;;
+  reload_share)
+    build_share
+    start_share
+    tail
+    ;;
+  reload_acs)
+    build_acs
+    start_acs
+    tail
+    ;;
+  build_test)
+    down
+    build
+    prepare_test
+    start
+    test
+    tail_all
+    down
+    ;;
+  test)
+    test
+    ;;
+  *)
+    echo "Usage: $0 {build_start|build_start_it_supported|start|stop|purge|tail|reload_share|reload_acs|build_test|test}"
+esac
